c988f4972cfb4cfaa527a0dbf667f2bf25bf1e8c
dvankevich <dvankevich@gmail.com>
2024-08-04 15:03:47 +0300

    create on click button handler

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,19 +6,28 @@ let promiceDelay = delay.value;
 
 delay.addEventListener('change', () => {
   promiceDelay = delay.value;
-  console.log(promiceDelay);
+  //console.log(promiceDelay);
 });
 
 const stateInputs = document.querySelectorAll('input[name="state"]');
 
 const handleClick = event => {
+  //console.log('--- submit ---');
   let promiceState = '';
   stateInputs.forEach(input => {
     if (input.checked) {
       promiceState = input.value;
     }
   });
-  console.log(promiceState, promiceDelay);
+  //console.log(promiceState, promiceDelay);
+  if (promiceState && promiceDelay) {
+    console.log(
+      `set promice with state: ${promiceState} and dellay: ${promiceDelay}`
+    );
+  } else {
+    window.alert('Please fill in all fields.');
+  }
 };
 
+//button.addEventListener('submit', handleClick);
 button.addEventListener('click', handleClick);
